c93e189222c7159bffc388bf6c2437037e011797
wangyuandong <chong2vv@163.com>
2022-10-31 18:22:22 +0800

    修改笔记位置这些

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
-int x = 1;
-int y = 2;
-int addtwonum();
-
-extern int count;
-extern void staticclass(void);
-
 int main() {
-    printf("Hello, World!\n");
-    int result = addtwonum();
-    printf("%d", result);
 
-    while (count--) {
-        staticclass();
+    for (int i = 0; i < 10; i++) {
+        printf("=====");
+        if(i == 5) {
+            break;
+        }
+        printf("%d", i);
     }
+
+    for (int i = 0; i < 10; i++) {
+        printf("+++++");
+        if(i == 5) {
+            continue;
+        }
+        printf("%d", i);
+    }
+
     return 0;
 }
